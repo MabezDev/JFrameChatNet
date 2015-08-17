c16dd96b77061e8b815d30a1c3cb1c8e00e74039
MabezDev <mabez123@gmail.com>
2015-08-17 02:13:52 +0100

    now running on invoke later queue to stop glitches on start up

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        new Clients("Scott","192.168.0.22",7777);
+
+
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println("Hello World!");
+                new Clients("Scott","192.168.0.22",7777);
+            }
+        });
     }
-}
+    }
+
+
